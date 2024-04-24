b335e3dbf0769ec2fc2037df3fa18b962f6f6f90
rod773 <rodrigoarcadio1@gmail.com>
2024-04-24 16:23:34 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,58 +1,59 @@
 import { useState, useEffect } from "react";
-import Map, { Marker } from "react-map-gl";
+import ReactMapGl, { GeolocateControl, NavigationControl } from "react-map-gl";
 import "mapbox-gl/dist/mapbox-gl.css";
-import { Geolocation } from "@capacitor/geolocation";
+import { Geolocation as geoloc } from "@capacitor/geolocation";
 
 const MAPBOX_TOKEN =
   "pk.eyJ1Ijoic2FoaWx0aGFrYXJlNTIxIiwiYSI6ImNrbjVvMTkzNDA2MXQydnM2OHJ6aHJvbXEifQ.z5aEqRBTtDMWoxVzf3aGsg";
 
-function App() {
+const App = () => {
   const [viewport, setViewport] = useState({});
 
   const currentPosition = async () => {
-    await Geolocation.getCurrentPosition().then((response) => {
+    await geoloc.getCurrentPosition().then((response) => {
       console.log(response);
       const latitude = response.coords.latitude;
       const longitude = response.coords.longitude;
       setViewport({
-        ...viewport,
-        latitude: latitude,
         longitude: longitude,
-        zoom: 15,
+        latitude: latitude,
+        zoom: 3.5,
       });
-    });
-  };
-
-  const permisions = async () => {
-    await Geolocation.checkPermissions().then((response) => {
-      console.log(response);
     });
   };
 
   useEffect(() => {
     currentPosition();
-    permisions();
   }, []);
 
   return (
-    <div>
-      {viewport.latitude && viewport.longitude && (
-        <div className="w-100 h-screen">
-          <h1>Your Location:</h1>
-          <Map
-            mapboxAccessToken={MAPBOX_TOKEN}
-            initialViewState={viewport}
-            mapStyle="mapbox://styles/mapbox/streets-v11"
-          >
-            <Marker
-              longitude={viewport.longitude}
-              latitude={viewport.latitude}
-            />
-          </Map>
+    <>
+      <div className="h-screen w-100">
+        <div className="text-center">
+          <div>lat: {viewport.latitude}</div>
+          <div>lon : {viewport.longitude}</div>
         </div>
-      )}
-    </div>
+        {viewport.latitude && (
+          <ReactMapGl
+            mapboxAccessToken={MAPBOX_TOKEN}
+            initialViewState={{
+              longitude: viewport.longitude,
+              latitude: viewport.latitude,
+              zoom: 14,
+            }}
+            onViewportChange={(viewport) => {
+              setViewport(...viewport);
+            }}
+            mapStyle={"mapbox://styles/mapbox/streets-v9"}
+            minZoom={5}
+          >
+            <NavigationControl />
+            <GeolocateControl />
+          </ReactMapGl>
+        )}
+      </div>
+    </>
   );
-}
+};
 
 export default App;
